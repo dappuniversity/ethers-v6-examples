7f205704a424b14bb7160e03cc31bcea68fb3585
Justin <justin7359900@gmail.com>
2025-05-02 12:20:17 -0400

    Setup starter code

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,15 +1,10 @@
-require("dotenv").config()
-const { ethers } = require("ethers")
+// Require packages
 
 // Setup connection
-const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-const provider = new ethers.JsonRpcProvider(URL)
-
-const ADDRESS = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e"
 
 async function main() {
-  const balance = await provider.getBalance(ADDRESS)
-  console.log(`\nETH Balance of ${ADDRESS} --> ${ethers.formatUnits(balance, 18)} ETH\n`)
+  // Get balance
+  // Log balance
 }
 
 main()
--- examples/2_send_signed_transaction.js
+++ examples/2_send_signed_transaction.js
@@ -5,35 +5,23 @@ const { ethers } = require("ethers")
 const { promptForKey } = require("../helpers/prompt.js")
 
 // Setup connection
-const URL = process.env.TENDERLY_RPC_URL
-const provider = new ethers.JsonRpcProvider(URL)
-
-const RECIEVER = "0xbd67bf269c42b8d8777351e4982dec68cee90a12" // Your account address 2
 
 async function main() {
   const privateKey = await promptForKey()
-  const wallet = new ethers.Wallet(privateKey, provider)
 
-  const senderBalanceBefore = await provider.getBalance(wallet.address)
-  const recieverBalanceBefore = await provider.getBalance(RECIEVER)
+  // Setup wallet
 
-  console.log(`\nSender balance before: ${ethers.formatUnits(senderBalanceBefore, 18)}`)
-  console.log(`Reciever balance before: ${ethers.formatUnits(recieverBalanceBefore, 18)}\n`)
+  // Get balances
 
-  const transaction = await wallet.sendTransaction({
-    to: RECIEVER,
-    value: ethers.parseUnits("1", 18)
-  })
+  // Log balances
 
-  const receipt = await transaction.wait()
-  console.log(transaction)
-  console.log(receipt)
+  // Create transaction
 
-  const senderBalanceAfter = await provider.getBalance(wallet.address)
-  const recieverBalanceAfter = await provider.getBalance(RECIEVER)
+  // Wait transaction
 
-  console.log(`\nSender balance after: ${ethers.formatUnits(senderBalanceAfter, 18)}`)
-  console.log(`Reciever balance after: ${ethers.formatUnits(recieverBalanceAfter, 18)}\n`)
+  // Get balances
+
+  // Log balances
 }
 
 main()
